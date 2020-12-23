e694d5649eb072b225b78b4aeb4d48b9b3238381
Justin Eldridge <eldridgejm@gmail.com>
2020-12-23 10:48:00 -0800

    Flaked.

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Python package for streamlining end-of-quarter grading.";
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/20.03;
+
+  outputs = { self, nixpkgs }: 
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+      {
+        gradelib = forAllSystems (system:
+          with import nixpkgs { system = "${system}"; };
+            python3Packages.buildPythonPackage rec {
+              name = "gradelib";
+              src = ./.;
+              propagatedBuildInputs = with python3Packages; [ pandas matplotlib numpy ];
+              # nativeBuildInputs = with python3Packages; [ black pytest jupyter ipython sphinx sphinx_rtd_theme ];
+              nativeBuildInputs = with python3Packages; [ black pytest ipython sphinx sphinx_rtd_theme ];
+            }
+          );
+
+        defaultPackage = forAllSystems (system:
+            self.gradelib.${system}
+          );
+      };
+
+}
--- shell.nix
+++
@@ -1,9 +0,0 @@
-with import <nixpkgs> {};
-with python37Packages;
-
-buildPythonPackage rec {
-  name = "gradelib";
-  src = ./.;
-  propagatedBuildInputs = [ pandas matplotlib numpy ];
-  nativeBuildInputs = [ black pytest jupyter ipython sphinx sphinx_rtd_theme ];
-}

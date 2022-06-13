4bc8505db2eaf094834b7b9a8326755b17543f36
Justin Eldridge <eldridgejm@gmail.com>
2022-06-12 21:27:36 -0700

    Added aarch64-darwin

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
 
   outputs = { self, nixpkgs }: 
     let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
     in
       {

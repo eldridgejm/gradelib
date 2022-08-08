95d453f9c1a559f32cb1a85d7c208bc4c0ceba12
Justin Eldridge <eldridgejm@gmail.com>
2022-08-08 15:50:01 -0500

    Added custom deduction for late.

--- tests/test_steps.py
+++ tests/test_steps.py
@@ -304,6 +304,29 @@ def test_penalize_lates_with_custom_penalty():
             "A2": {"hw01": [Points(3)]},
     }
 
+def test_penalize_lates_respects_lateness_fudge():
+    # given
+    columns = ["hw01", "hw02", "lab01"]
+    p1 = pd.Series(data=[30, 90, 20], index=columns, name="A1")
+    p2 = pd.Series(data=[7, 15, 20], index=columns, name="A2")
+    points_marked = pd.DataFrame([p1, p2])
+    points_possible = pd.Series([50, 100, 20], index=columns)
+    lateness = pd.DataFrame([
+            pd.to_timedelta([0, 0, 50], 's'),
+            pd.to_timedelta([6000, 0, 0], 's')
+        ], columns=columns, index=points_marked.index)
+    gradebook = gradelib.MutableGradebook(points_marked, points_possible, lateness=lateness)
+
+    HOMEWORK = gradebook.assignments.starting_with("hw")
+
+    result = gradebook.apply(
+        gradelib.steps.PenalizeLates()
+    )
+
+    assert result.deductions == {
+            "A2": {"hw01": [Points(3)]},
+    }
+
 def test_penalize_lates_within_assignments():
     # given
     columns = ["hw01", "hw02", "lab01"]

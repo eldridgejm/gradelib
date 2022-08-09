a95b4152244c78256f2d731c6f2179c835c4d840
Justin Eldridge <eldridgejm@gmail.com>
2022-08-09 11:42:49 -0500

    Moved CombineAssignment back to method.

--- gradelib/core.py
+++ gradelib/core.py
@@ -585,6 +585,39 @@ def combine_gradebooks(gradebooks, restricted_to_pids=None):
     )
 
 
+def _combine_and_convert_deductions(parts, new_name, deductions, points_possible):
+    """Concatenates all deductions from the given parts.
+
+    Converts percentage deductions to points deductions along the way.
+
+    Used in MutableGradebook._combine_assignment
+
+    """
+
+    # combine and convert deductions
+    def _convert_deduction(assignment, deduction):
+        if isinstance(deduction, Percentage):
+            possible = points_possible.loc[assignment]
+            return Points(possible * deduction.amount)
+        else:
+            return deduction
+
+    new_deductions = {}
+    for student, assignments_dct in deductions.items():
+        new_deductions[student] = {}
+
+        combined_deductions = []
+        for assignment, deductions_lst in assignments_dct.items():
+            deductions_lst = [_convert_deduction(assignment, d) for d in deductions_lst]
+            if assignment in parts:
+                combined_deductions.extend(deductions_lst)
+            else:
+                new_deductions[student][assignment] = deductions_lst
+
+        new_deductions[student][new_name] = combined_deductions
+
+    return new_deductions
+
 class MutableGradebook(Gradebook):
     """A gradebook with methods for changing assignments and grades."""
 
@@ -770,6 +803,120 @@ class MutableGradebook(Gradebook):
         return self._replace(
             points_marked=r_points, lateness=r_lateness, dropped=r_dropped
         )
+
+
+    def _combine_assignment(self, new_name, parts):
+        """A helper function to combine assignments under the new name."""
+        parts = list(parts)
+        if self.dropped[parts].any(axis=None):
+            raise ValueError("Cannot combine assignments with drops.")
+
+        assignment_points = self.points_marked[parts].sum(axis=1)
+        assignment_max = self.points_possible[parts].sum()
+        assignment_lateness = self.lateness[parts].max(axis=1)
+
+        new_points = self.points_marked.copy().drop(columns=parts)
+        new_max = self.points_possible.copy().drop(parts)
+        new_lateness = self.lateness.copy().drop(columns=parts)
+
+        new_points[new_name] = assignment_points
+        new_max[new_name] = assignment_max
+        new_lateness[new_name] = assignment_lateness
+
+        # combines deductions from all of the parts, converting Percentage
+        # to Points along the way.
+        new_deductions = _combine_and_convert_deductions(
+            parts, new_name, self.deductions, self.points_possible
+        )
+
+        # we're assuming that dropped was not set; we need to provide an empy
+        # mask here, else ._replace will use the existing larger dropped table
+        # of self, which contains all parts
+        new_dropped = _empty_mask_like(new_points)
+
+        return self._replace(
+            points_marked=new_points,
+            points_possible=new_max,
+            dropped=new_dropped,
+            lateness=new_lateness,
+            deductions=new_deductions,
+        )
+
+
+    def with_assignments_combined(self, dct_or_callable):
+        """Combine the assignment parts into one single assignment with the new name.
+
+        Sometimes assignments may have several parts which are recorded separately
+        in the grading software. For instance, a homework might
+        have a written part and a programming part. This method makes it easy
+        to combine these parts into a single assignment.
+
+        The individual assignment parts are removed from the gradebook.
+
+        The new marked points and possible points are calculated by addition.
+        The lateness of the new assignment is the *maximum* lateness of any of
+        its parts.
+
+        Deductions are concatenated. Points are propagated unchanged, but
+        Percentage objects are converted to Points according to the ratio of
+        the part's value to the total points possible. For example, if the
+        first part is worth 70 points, and the second part is worth 30 points,
+        and a 25% Percentage is applied to the second part, it is converted to
+        a 25% * 30 = 7.5 point Points.
+
+        It is unclear what the result should be if any of the assignments to be
+        unified has been dropped, but other parts have not. For this reason,
+        this method will raise a `ValueError` if *any* of the parts have been
+        dropped.
+
+        Parameters
+        ----------
+        dct_or_callable : Mapping[str, Collection[str]]
+            Either: 1) a mapping whose keys are new assignment names, and whose
+            values are collections of assignments that should be unified under
+            their common key; or 2) a callable which maps assignment names to
+            new assignment by which they should be grouped.
+
+        Raises
+        ------
+        ValueError
+            If any of the assignments to be unified is marked as dropped. See above for
+            rationale.
+
+        Example
+        -------
+
+        Assuming the gradebook has assignments named `homework 01`, `homework 01 - programming`,
+        `homework 02`, `homework 02 - programming`, etc., the following will "combine" the
+        assignments into `homework 01`, `homework 02`, etc:
+
+            >>> gradebook.apply(CombineAssignments(lambda s: s.split('-')[0].strip()))
+
+        Alternatively, you could write:
+
+            >>> gradebook.apply(CombineAssignments({
+                'homework 01': {'homework 01', 'homework 01 - programming'},
+                'homework 02': {'homework 02', 'homework 02 - programming'}
+                }))
+
+        """
+
+        if not callable(dct_or_callable):
+            dct = dct_or_callable
+        else:
+            to_key = dct_or_callable
+            dct = {}
+            for assignment in self.assignments:
+                key = to_key(assignment)
+                if key not in dct:
+                    dct[key] = []
+                dct[key].append(assignment)
+
+        result = self
+        for key, value in dct.items():
+            result = result._combine_assignment(key, value)
+
+        return result
 
     def add_note(self, pid, channel, message):
         """Convenience method for adding a note.

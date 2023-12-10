5ed086aa20f0b1e27d8799fc2308582efa61f3ec
Justin Eldridge <eldridgejm@gmail.com>
2023-12-10 12:51:36 -0800

    Cleaned up LSP diagnostics.

--- gradelib/_examples.py
+++ gradelib/_examples.py
@@ -2,7 +2,6 @@
 
 import gradelib
 
-import numpy as np
 import pandas as pd
 
 
--- gradelib/policies/__init__.py
+++ gradelib/policies/__init__.py
@@ -1,1 +1,3 @@
 from . import exceptions, lates, drops, attempts
+
+__all__ = ["exceptions", "lates", "drops", "attempts"]

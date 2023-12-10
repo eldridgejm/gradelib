5b8350419661e5c94cdb6eff3def486b5072a63f
Justin Eldridge <eldridgejm@gmail.com>
2023-12-09 22:39:48 -0800

    Simplified code.

--- gradelib/policies/attempts.py
+++ gradelib/policies/attempts.py
@@ -40,6 +40,37 @@ class Maximum:
             gradebook.add_note(student, "attempts", make_note_for(student))
 
 
+def _scores_after_penalty(
+    raw_scores: _pd.Series, policy: Callable[[int, float], float]
+):
+    """Apply a penalty policy to a series of scores."""
+    return _pd.Series(
+        [policy(i, score) for i, score in enumerate(raw_scores)], index=raw_scores.index
+    )
+
+
+def _make_notes(raw_scores: _pd.Series, effective_scores: _pd.Series) -> str:
+    """Make a string to be used as a note for a student."""
+    parts = []
+    for assignment, raw_score, effective_score in zip(
+        raw_scores.index, raw_scores, effective_scores
+    ):
+        raw_score = _fmt_as_pct(raw_score)
+        effective_score = _fmt_as_pct(effective_score)
+        if raw_score == effective_score:
+            parts.append(f"{str(assignment).title()} score: {raw_score}.")
+        else:
+            parts.append(
+                f"{str(assignment).title()} raw score: {raw_score}, after penalty for retrying: {effective_score}."
+            )
+
+    best_assignment = str(effective_scores.idxmax())
+    best_score = _fmt_as_pct(effective_scores.max())
+    parts.append(f"{best_assignment.title()} score ({best_score}) used.")
+
+    return " ".join(parts)
+
+
 def take_best(
     gradebook: Gradebook,
     attempts: Mapping[str, Sequence[str]],
@@ -75,66 +106,16 @@ def take_best(
     for new_assignment, existing_assignments in attempts.items():
         best_scores = _pd.Series()
         for student in gradebook.students:
-            effective_scores = _pd.Series()
-            notes = []
-            for i, attempt in enumerate(existing_assignments):
-                raw_score = gradebook.score.loc[student, attempt]
-                effective_score = policy(i, raw_score)
+            raw_scores = gradebook.score.loc[student, existing_assignments]
+            effective_scores = _scores_after_penalty(raw_scores, policy)
 
-                if raw_score == effective_score:
-                    notes.append(f"{attempt.title()} score: {_fmt_as_pct(raw_score)}.")
-                else:
-                    notes.append(
-                        f"{attempt.title()} raw score: {_fmt_as_pct(raw_score)}, "
-                        f"after penalty for retrying: {_fmt_as_pct(effective_score)}."
-                    )
-
-                effective_scores.loc[attempt] = effective_score
+            gradebook.add_note(
+                student, "attempts", _make_notes(raw_scores, effective_scores)
+            )
 
             best_scores[student] = effective_scores.max()
-            best_assignment = str(effective_scores.idxmax())
-            notes.append(
-                f"{best_assignment.title()} score ({_fmt_as_pct(best_scores[student])}) used."
-            )
-            gradebook.add_note(student, "attempts", " ".join(notes))
 
         points_earned = best_scores * points_possible
-        gradebook.add_assignment(new_assignment, points_earned, points_possible)
-
-        if remove:
-            gradebook.remove_assignments(existing_assignments)
-
-
-def take_best_old(
-    gradebook: Gradebook,
-    attempts: Mapping[str, Sequence[str]],
-    *,
-    remove=True,
-    policy: Callable[[Gradebook, Sequence[str]], _pd.Series] = Maximum(),
-    points_possible: Union[int, float] = 1.0,
-):
-    """Replaces multiple attempts at an assignment with the best.
-
-    Parameters
-    ----------
-    gradebook : Gradebook
-        The gradebook that will be modified.
-    attempts : Mapping[str, Sequence[str]]
-        A mapping from the name of the new assignment to a sequence of
-        existing assignments that will be used to determine the score on
-        the new assignment.
-    remove : bool, optional
-        Whether to remove the existing assignments, by default True.
-    policy : Callable[[Gradebook, Sequence[str]], pd.Series], optional
-        A function that takes a gradebook and a sequence of assignment names
-        and returns a Series of scores, by default Maximum().
-    points_possible : Union[int, float], optional
-        The number of points possible on the new assignment, by default 1.
-
-    """
-    for new_assignment, existing_assignments in attempts.items():
-        redeemed_score = policy(gradebook, existing_assignments)
-        points_earned = redeemed_score * points_possible
         gradebook.add_assignment(new_assignment, points_earned, points_possible)
 
         if remove:
